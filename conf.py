c4b3d21951f332efd430b4b47f15ece4e772ae89
YvonneKallberg <yvonne.kallberg@scilifelab.se>
2019-12-04 10:47:23 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ extensions = [
 ]
 
 source_parsers = {
-    '.md': 'recommonmark.parser.CommonMarkParser',
+    '.md': 'CommonMarkParser',
 }
 
 # Add any paths that contain templates here, relative to this directory.
